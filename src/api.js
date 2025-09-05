8c75628f09daaba5fd30596c565041ce98ee9ca4
ShivSinghB <Shivbaghel656@gmail.com>
2025-09-05 13:22:16 +0530

    API PROBLEM SOLVED

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API = axios.create({ baseURL: "http://localhost:5000/api" });
-const API = axios.create({ baseURL: import.meta.env.API_BASE_URL });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL });
 
 // Automatically add token if available
 API.interceptors.request.use((req) => {
